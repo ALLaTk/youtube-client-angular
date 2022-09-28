3ece438ec4e40d9d64957f7595ba86989beeb0f6
Alla <regicter1810@mail.ru>
2022-09-28 19:35:34 +0300

    feat: add components, models and mock

--- youtube-client/src/app/app.module.ts
+++ youtube-client/src/app/app.module.ts
@@ -3,9 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { SearchItemComponent } from './search/search-item/search-item.component';
+import { SearchResultsComponent } from './search/search-results/search-results.component';
+import { SearchSortingComponent } from './search/search-sorting/search-sorting.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    SearchItemComponent,
+    SearchResultsComponent,
+    SearchSortingComponent,
+  ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent],
---
+++ youtube-client/src/app/search/search-sorting/search-sorting.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-search-sorting',
+  templateUrl: './search-sorting.component.html',
+  styleUrls: ['./search-sorting.component.scss'],
+})
+export class SearchSortingComponent {}
