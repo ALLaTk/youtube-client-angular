a9e5a8a8ae2d36d867da21e5c12f2245f8fa0072
Alla <regicter1810@mail.ru>
2022-10-02 17:27:58 +0300

    feat: add a directive to change background color of the card border

--- youtube-client/src/app/search/search-item/search-item.component.ts
+++ youtube-client/src/app/search/search-item/search-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 import { SearchItem } from '../models/search-item.model';
 
 @Component({
@@ -6,7 +6,36 @@ import { SearchItem } from '../models/search-item.model';
   templateUrl: './search-item.component.html',
   styleUrls: ['./search-item.component.scss'],
 })
-export class SearchItemComponent {
+export class SearchItemComponent implements OnInit {
   @Input()
   public item!: SearchItem;
+
+  private colorsBorderObj: { [key: string]: string } = {
+    red: '#EB5757',
+    yellow: '#F2C94C',
+    green: '#27AE60',
+    blue: '#2F80ED',
+  };
+
+  public colorBottom: string = '';
+
+  ngOnInit(): void {
+    this.choiceColorBottom();
+  }
+
+  private choiceColorBottom(): void {
+    const nowDate: Date = new Date();
+    const pastDate: Date = new Date(this.item.snippet.publishedAt);
+    const diff = Math.floor(nowDate.getTime() - pastDate.getTime());
+    const day = 1000 * 60 * 60 * 24;
+    const days = Math.floor(diff / day);
+
+    if (days >= 31 && days <= 30 * 6) {
+      this.colorBottom = this.colorsBorderObj['yellow'];
+    } else if (days >= 7 && days <= 30) {
+      this.colorBottom = this.colorsBorderObj['green'];
+    } else if (days < 7) {
+      this.colorBottom = this.colorsBorderObj['blue'];
+    } else this.colorBottom = this.colorsBorderObj['red'];
+  }
 }
