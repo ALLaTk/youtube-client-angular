8800255156bcd01b62ec7e1bf04da305d833be78
Alla <regicter1810@mail.ru>
2022-10-02 18:20:54 +0300

    feat: add a method showFilterBlock to show or hide the filter block for clicking on the settings button

--- youtube-client/src/app/header/header.component.ts
+++ youtube-client/src/app/header/header.component.ts
@@ -8,5 +8,10 @@ import { Component } from '@angular/core';
 export class HeaderComponent {
   public placeholderText: string = 'What are you want to find out?';
 
-  public path: string = '../../assets/svg/logo.svg';
+  public flagForRenderFilterBlock: boolean = false;
+
+  public showFilterBlock() {
+    if (this.flagForRenderFilterBlock) this.flagForRenderFilterBlock = false;
+    else this.flagForRenderFilterBlock = true;
+  }
 }
