22c8e78271eb767aba1604cc7f839479e4bfe12f
Alla <regicter1810@mail.ru>
2022-10-10 16:06:03 +0300

    feat: add core module

--- youtube-client/src/app/app.module.ts
+++ youtube-client/src/app/app.module.ts
@@ -1,29 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
-import { NoopAnimationsModule } from '@angular/platform-browser/animations';
 import { MatButtonModule } from '@angular/material/button';
 import { AppRoutingModule } from './app-routing.module';
 import { SharedModule } from './shared/shared.module';
 import { AppComponent } from './app.component';
 import { SearchItemComponent } from './components/search-results/search-item/search-item.component';
 import { SearchResultsComponent } from './components/search-results/search-results.component';
-import { HeaderComponent } from './components/header/header.component';
-import { FilterComponent } from './components/filter/filter.component';
+import { CoreModule } from './core/core.module';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    HeaderComponent,
-    FilterComponent,
-    SearchItemComponent,
-    SearchResultsComponent,
-  ],
+  declarations: [AppComponent, SearchItemComponent, SearchResultsComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    NoopAnimationsModule,
     MatButtonModule,
+    CoreModule,
     SharedModule,
   ],
   providers: [],
---
+++ youtube-client/src/app/core/core.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './components/header/header.component';
+import { FilterComponent } from '../components/filter/filter.component';
+
+@NgModule({
+  declarations: [HeaderComponent, FilterComponent],
+  exports: [HeaderComponent, FilterComponent],
+  imports: [CommonModule],
+})
+export class CoreModule {}
