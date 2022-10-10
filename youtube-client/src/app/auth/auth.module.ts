0da8543deac32a79aa1dea881e29dc787ee628ce
Alla <regicter1810@mail.ru>
2022-10-10 19:59:48 +0300

    feat: add auth module

---
+++ youtube-client/src/app/auth/auth.module.ts
@@ -0,0 +1,12 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { AuthRoutingModule } from './auth-routing.module';
+import { AuthComponent } from './components/auth/auth.component';
+
+@NgModule({
+  declarations: [AuthComponent],
+  exports: [AuthComponent],
+  imports: [CommonModule, AuthRoutingModule],
+})
+export class AuthModule {}
