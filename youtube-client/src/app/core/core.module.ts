94c78eb9d3a64aa988e29288e4307370ade70e75
Alla <regicter1810@mail.ru>
2022-10-17 14:52:42 +0300

    feat: add api-key and api-url interceptors

--- youtube-client/src/app/core/core.module.ts
+++ youtube-client/src/app/core/core.module.ts
@@ -1,16 +1,33 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { HeaderComponent } from './components/header/header.component';
 import { SharedModule } from '../shared/shared.module';
 import { HeaderService } from './services/header.service';
 import { AuthService } from '../auth/services/auth.service';
 import { AuthGuard } from './guards/auth.guard';
 import { NotFoundPageComponent } from './pages/not-found-page/not-found-page.component';
+import { ApiKeyInterceptor } from './interceptor/api-key.interceptor';
+import { ApiUrlInterceptor } from './interceptor/api-url.interceptor';
 
 @NgModule({
   declarations: [HeaderComponent, NotFoundPageComponent],
   exports: [HeaderComponent],
   imports: [CommonModule, SharedModule],
-  providers: [HeaderService, AuthService, AuthGuard],
+  providers: [
+    HeaderService,
+    AuthService,
+    AuthGuard,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: ApiKeyInterceptor,
+      multi: true,
+    },
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: ApiUrlInterceptor,
+      multi: true,
+    },
+  ],
 })
 export class CoreModule {}
---
+++ youtube-client/src/app/core/interceptor/api-key.interceptor.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import {
+  HttpRequest,
+  HttpHandler,
+  HttpEvent,
+  HttpInterceptor,
+} from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class ApiKeyInterceptor implements HttpInterceptor {
+  private key: string = 'AIzaSyDhXUPaDxozUfQJDU_0StBmmCfc8USqctU';
+
+  intercept(
+    request: HttpRequest<any>,
+    next: HttpHandler,
+  ): Observable<HttpEvent<any>> {
+    const apiRequest = request.clone({
+      params: request.params.append('key', this.key),
+    });
+    return next.handle(apiRequest);
+  }
+}
